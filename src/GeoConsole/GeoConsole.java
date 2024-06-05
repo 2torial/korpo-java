90ec5d7c12ea8830d2d0bd53ecf0a1c6f21166d9
Daniel <315942@uwr.edu.pl>
2024-06-05 10:04:31 +0200

    Translated main classes
    GeoConsole, Argument, Command, CommandFactory

--- src/GeoConsole/GeoConsole.java
+++ src/GeoConsole/GeoConsole.java
@@ -1,6 +1,9 @@
 package GeoConsole;
 
 import GeoConsole.UserInput.*;
+import GeoConsole.UserInput.Context.Translator.Identifier;
+import GeoConsole.UserInput.Context.Translator.Lang;
+import GeoConsole.UserInput.Context.Translator.Translator;
 import GeoConsole.UserInput.Exceptions.DuplicateParameterException;
 import GeoConsole.UserInput.Exceptions.InvalidNumberOfArguments;
 import GeoConsole.UserInput.Exceptions.InvalidParameterException;
@@ -9,10 +12,21 @@ import GeoConsole.UserInput.Exceptions.InvalidPositionException;
 import java.util.Scanner;
 
 public class GeoConsole{
+    static {
+        Translator.save(Lang.PL, Identifier.ERR_EXITED,
+                "Niemożliwy stan: zamknięto konsolę");
+        Translator.save(Lang.EN, Identifier.ERR_EXITED,
+                "Console is exited");
+
+        Translator.save(Lang.PL, Identifier.ERR_EMPTY_INPUT,
+                "Wprowadzone dane nie mogą być puste");
+        Translator.save(Lang.EN, Identifier.ERR_EMPTY_INPUT,
+                "Input cannot be empty");
+    }
     private final Scanner scanner = new Scanner(System.in);
 
     private boolean isExited = false;
-    public final static String versionInfo = "GeoConsole ver 1.0P\nRelease date: 15.05.2024\n";
+    public final static String versionInfo = "GeoConsole v1.0 (05.06.2024)";
 
     public void exit() {
         isExited = true;
@@ -24,11 +38,11 @@ public class GeoConsole{
 
     public void handleInput() throws InvalidParameterException, InvalidNumberOfArguments, DuplicateParameterException, InvalidPositionException {
         if (isExited)
-            throw new IllegalStateException("Console is exited");
+            throw new IllegalStateException(Translator.read(Identifier.ERR_EXITED));
 
         String[] tokens = scanner.nextLine().trim().toLowerCase().split("\\s+");
         if (tokens.length == 0)
-            throw new IllegalArgumentException("Input cannot be empty");
+            throw new IllegalArgumentException(Translator.read(Identifier.ERR_EMPTY_INPUT));
 
         Command command = CommandFactory.parseCommand(tokens);
         if (command.getName().equals("exit")) {
--- src/GeoConsole/UserInput/CommandFactory.java
+++ src/GeoConsole/UserInput/CommandFactory.java
@@ -2,6 +2,9 @@ package GeoConsole.UserInput;
 
 import GeoConsole.UserInput.Commands.*;
 import GeoConsole.UserInput.Commands.Figures.*;
+import GeoConsole.UserInput.Context.Translator.Identifier;
+import GeoConsole.UserInput.Context.Translator.Lang;
+import GeoConsole.UserInput.Context.Translator.Translator;
 import GeoConsole.UserInput.Exceptions.*;
 
 import java.util.HashMap;
@@ -10,19 +13,30 @@ import java.util.function.BiConsumer;
 import java.util.function.Supplier;
 
 public final class CommandFactory {
+    static {
+        Translator.save(Lang.PL, Identifier.ERR_UNKNOWN_COMMAND,
+                "Nieznane polecenie: ");
+        Translator.save(Lang.EN, Identifier.ERR_UNKNOWN_COMMAND,
+                "Unknown command: ");
+
+        Translator.save(Lang.PL, Identifier.ERR_EMPTY_ARGUMENT,
+                "Argument nie może być pusty");
+        Translator.save(Lang.EN, Identifier.ERR_EMPTY_ARGUMENT,
+                "Argument cannot be empty");
+    }
     private CommandFactory() {} // Static class
 
     public static Command parseCommand(String[] tokens) throws InvalidParameterException, DuplicateParameterException {
         String name = tokens[0];
         Supplier<Command> supplier = commandSuppliers.get(name);
         if (supplier == null)
-            throw new IllegalArgumentException("Unknown command: " + name);
+            throw new IllegalArgumentException(Translator.read(Identifier.ERR_UNKNOWN_COMMAND) + name);
         var command = supplier.get();
 
         for (int position = 1; position < tokens.length; position++) {
             String token = tokens[position];
             if (token == null || token.isEmpty())
-                throw new IllegalArgumentException("Argument cannot be empty");
+                throw new IllegalArgumentException(Translator.read(Identifier.ERR_EMPTY_ARGUMENT));
             if (token.length() == 1 || token.charAt(0) != '-' || token.charAt(1) != '-') {
                 command.addArgument(new Argument(token, position, false));
                 continue;
